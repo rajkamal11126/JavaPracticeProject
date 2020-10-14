000accbadc7f0c0a16467546063eb6abdac0b88d
Raj <rajkamal11126@gmail.com>
2020-10-14 13:53:04 +0530

    Added: HelloWorld message

---
+++ src/main/java/com/JavaPractice/App.java
@@ -0,0 +1,24 @@
+package com.JavaPractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	String message = "Hello, World";
+    	LOG.debug(message + "Will be Printed on Debug");
+    	LOG.info(message + "Will be Printed on Info");
+    	LOG.warn(message + "Will be Printed on Warn");
+    	LOG.error(message + "Will be Printed on Error");
+    	LOG.fatal(message + "Will be Printed on Fatal");
+    	LOG.info("Appending string: {}.", message);
+        System.out.println(message);
+    }
+}
